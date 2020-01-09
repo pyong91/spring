54130c9ea1360cc7447014f9c049cb52bcb8a124
haggismagic <56706690+haggismagic@users.noreply.github.com>
2020-01-09 18:25:26 +0900

    spring

---
+++ sts04/src/main/java/com/kh/sts04/FirstController.java
@@ -0,0 +1,19 @@
+package com.kh.sts04;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class FirstController implements Controller{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("first");
+		
+		return mv;
+	}
+	
+}
---
+++ sts04/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<a href="${pageContext.request.contextPath}/first">first로 이동</a>	
+	<a href="first">first로 이동</a>	
+	<a href="${pageContext.request.contextPath}/second">second로 이동</a>	
+	<a href="second">second로 이동</a>	
+</body>
+</html>
